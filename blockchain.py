7b527e1786d13a47efbc74a08eaef48e7f89840e
chukkyiii <jgodwin1420@gmail.com>
2022-01-20 16:05:00 +0000

    first commit

---
+++ blockchain.py
@@ -0,0 +1,22 @@
+class Blockchain(object):
+    def __init__(self): 
+        self.chain = []
+        self.self.current_transactions = []
+
+    def new_block(self):
+        # Creates a new block and adds to chain
+        pass 
+
+    def new_transaction(self):
+        # Adds a new transaction to the list of transactions
+        pass 
+
+    @staticmethod
+    def hash(block):
+        # Hashes a block
+        pass 
+
+    def last_block(self):
+        # returns the last block of the chain.  
+        pass 
+
