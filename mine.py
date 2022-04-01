7eb8c7ceeb291394a33187ca585b68a4ab3b70fb
Jesse <c1022253@ncl.ac.uk>
2022-04-01 01:33:57 +0100

    made port argument

--- mine.py
+++ mine.py
@@ -1,8 +1,15 @@
 import requests as r
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
+args = parser.parse_args()   
+port = args.port
+
 print('...Mining...')
 while 1:
-    r.get('http://127.0.0.1:5000/mine')
-    amount = r.get('http://127.0.0.1:5000/amount').json()
+    r.get(f'http://127.0.0.1:{port}/mine')
+    amount = r.get(f'http://127.0.0.1:{port}/amount').json()
 
     if amount['amount'] % 100 == 0:
         print(f'{amount["amount"]}th  has been reached')
